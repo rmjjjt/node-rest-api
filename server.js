c16e612e150acc9f7306f5abd35a93460c6ca5bb
Rhymes Toasterface <rhyme_master@hotmail.co.uk>
2017-03-01 11:16:18 +0000

    Add route to create bear using POST at api/bears

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 // Base setup
 
+var mongoose   = require('mongoose');
+mongoose.connect('mongodb://localhost/bears'); // connect to our database
+
 // Call the packages we need
 
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var Bear = require('./app/models/bear');
 
 // configure app to use body parser
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -16,12 +20,38 @@ var port = process.env.PORT || 8080;  //set port
 
 var router = express.Router();
 
+// Middleware to use for all requests
+router.use(function(req, res, next) {
+    // do logging
+    console.log('Something is happening!');
+    next(); // make sure we go to the next routes and don't stop here
+});
+
 // test route to make sure it is working
 router.get('/', function(req, res) {
-    res.json({ message: 'hooray! Welcome to the api!' })
+    res.json({ message: 'Hooray! Welcome to the api!' })
 });
 
 // more API routes here
+
+// on routes that end in /bears
+router.route('/bears')
+
+    // create a bear /api/bears POST
+    .post(function(req, res) {
+
+        var bear = new Bear(); // create a new instance of Bear model
+        bear.name = req.body.name; // set the bear's name (from the request)
+
+        // save the bear and check for errors
+        bear.save(function(err) {
+            if(err)
+                res.send(err);
+
+            res.json({ message: 'Bear created'});
+        });
+
+    });
 
 //REGISTER ROUTES
 // all routes will be prefixed with /api
